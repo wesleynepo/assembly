fa4b838a90ae052f97e5841dc72b8f23670604a7
Wesley Nepomuceno <wesleynepomuceno1@gmail.com>
2021-07-04 22:38:28 -0300

    feat: counting function and algorithm, ASCII printing

--- functions.asm
+++ functions.asm
@@ -1,3 +1,49 @@
+; iprint ( integer )'
+iprint:
+  push eax 
+  push ecx 
+  push edx 
+  push esi 
+  mov  ecx, 0
+
+divideLoop:
+  inc  ecx
+  mov  edx, 0
+  mov  esi, 10
+  idiv esi 
+  add  edx, 48
+  push edx
+  cmp  eax, 0
+  jnz divideLoop
+
+printLoop:
+  dec  ecx
+  mov  eax, esp 
+  call sprint
+  pop  eax
+  cmp  ecx, 0 
+  jnz  printLoop
+
+  pop esi 
+  pop edx 
+  pop ecx 
+  pop eax 
+  ret
+  
+; iprintlF
+
+iprintLF:
+  call iprint
+
+  push eax
+  mov  eax, 0x0a
+  push eax
+  mov  eax, esp
+  call sprint
+  pop  eax
+  pop  eax
+  ret 
+
 ; int slen (string message)
 ; String lenght
 slen:
