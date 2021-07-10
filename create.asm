39dee5961a6b5ce317a4e14e8c8c02bd4f818f8a
Wesley Nepomuceno <wesleynepomuceno1@gmail.com>
2021-07-10 20:24:38 -0300

    feat: file creation with sys_creat

---
+++ create.asm
@@ -0,0 +1,16 @@
+%include 'functions.asm'
+
+SECTION .data
+filename db 'readme.txt',
+
+SECTION .text
+global _start
+
+_start: 
+
+  mov ecx, 0777
+  mov ebx, filename
+  mov eax, 8
+  int 80h
+
+  call quit 
