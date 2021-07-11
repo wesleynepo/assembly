a4547733c55bce993b47de67883d8b63548f8e26
Wesley Nepomuceno <wesleynepomuceno1@gmail.com>
2021-07-10 22:37:20 -0300

    feat: bind to ip

--- socket.asm
+++ socket.asm
@@ -20,7 +20,20 @@ _socket:
   mov eax, 102
   int 80h
 
-  call iprintLF
+_bind:
+
+  mov edi, eax 
+  push dword 0x00000000
+  push word 0x2923
+  push word 2
+  mov ecx, esp 
+  push byte 16
+  push ecx
+  push edi
+  mov ecx, esp
+  mov ebx, 2
+  mov eax, 102
+  int 80h
 
 _exit:
 
